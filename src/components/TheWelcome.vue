d2dcc8e83ae7fd6a9acceb35b9555319d1f6c0bf
TiaFreeman <tiajane.freeman@gmail.com>
2023-02-24 18:48:31 +0900

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// import DocumentationIcon from './icons/IconDocumentation.vue'
+// import EcosystemIcon from './icons/IconEcosystem.vue'
+// import CommunityIcon from './icons/IconCommunity.vue'
+// import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <h1>Hello</h1>
+</template>
