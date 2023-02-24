858c14c7527f8b24884daa8be3d5506094af478a
TiaFreeman <tiajane.freeman@gmail.com>
2023-02-24 19:21:03 +0900

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/"></RouterLink>
       </nav>
     </div>
   </header>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-// import DocumentationIcon from './icons/IconDocumentation.vue'
-// import EcosystemIcon from './icons/IconEcosystem.vue'
-// import CommunityIcon from './icons/IconCommunity.vue'
-// import SupportIcon from './icons/IconSupport.vue'
+
 </script>
 
 <template>
-  <h1>Hello</h1>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
